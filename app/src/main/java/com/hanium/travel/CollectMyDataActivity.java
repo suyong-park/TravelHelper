469625dab3a8ec5e31d3f60b42ab9a890c2eec70
suyong-park <spdlqjfire@mju.ac.kr>
2021-08-27 20:53:19 +0900

    bug fix, modify ui

--- app/src/main/java/com/hanium/travel/CollectMyDataActivity.java
+++ app/src/main/java/com/hanium/travel/CollectMyDataActivity.java
@@ -1,22 +1,30 @@
 package com.hanium.travel;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.hanium.travel.fragment.MyData0Fragment;
+import com.hanium.travel.fragment.MyData1Fragment;
 import com.hanium.travel.fragment.MyData2Fragment;
 import com.hanium.travel.fragment.MyData3Fragment;
+import com.shashank.sony.fancydialoglib.Animation;
+import com.shashank.sony.fancydialoglib.FancyAlertDialog;
 
 public class CollectMyDataActivity extends AppCompatActivity {
 
     private Button next_btn;
     private Button back_btn;
+    MyData1Fragment myData1Fragment;
+    MyData2Fragment myData2Fragment;
+    MyData3Fragment myData3Fragment;
 
     private int pageNum = 0;
 
@@ -30,42 +38,104 @@ public class CollectMyDataActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
         MyData0Fragment myData0Fragment = new MyData0Fragment();
         fragmentTransaction.add(R.id.mydata_frame, myData0Fragment).commit();
 
-        next_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        back_btn.setOnClickListener(view -> {
 
-                pageNum++;
+            FragmentManager fragmentManagerBack = getSupportFragmentManager();
+            FragmentTransaction fragmentTransactionBack = fragmentManagerBack.beginTransaction();
 
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-                if(pageNum == 2)
-                    next_btn.setText("시작");
-
-                if(pageNum > 2) {
-                    Intent intent = new Intent(CollectMyDataActivity.this, MainActivity.class);
-                    startActivity(intent);
+            pageNum--;
+            if(pageNum < 0) {
+                pageNum = 0;
+                myData1Fragment = new MyData1Fragment();
+                fragmentTransactionBack.remove(myData1Fragment);
+                while (fragmentManagerBack.getBackStackEntryCount() != 0) {
+                    fragmentManagerBack.popBackStackImmediate();
                 }
+                fragmentTransactionBack.commit();
 
-                switch (pageNum) {
-                    case 1 :
-                        MyData2Fragment myData2Fragment = new MyData2Fragment();
-                        fragmentTransaction.replace(R.id.mydata_frame, myData2Fragment);
-                        fragmentTransaction.addToBackStack(null);
-                        fragmentTransaction.commitAllowingStateLoss();
-                        break;
-                    case 2 :
-                        MyData3Fragment myData3Fragment = new MyData3Fragment();
-                        fragmentTransaction.replace(R.id.mydata_frame, myData3Fragment);
-                        fragmentTransaction.addToBackStack(null);
-                        fragmentTransaction.commitAllowingStateLoss();
-                        break;
-                }
+                next_btn.setVisibility(View.GONE);
+                back_btn.setVisibility(View.GONE);
+
+                return;
+            }
+
+            if(pageNum != 2)
+                next_btn.setText("다음");
+
+            System.out.println("이전 버튼 페이지 번호 : " + pageNum);
+
+            switch (pageNum) {
+                case 0 :
+                    myData1Fragment = new MyData1Fragment();
+                    fragmentTransactionBack.replace(R.id.mydata_frame, myData1Fragment);
+                    fragmentTransactionBack.addToBackStack(null);
+                    fragmentTransactionBack.commitAllowingStateLoss();
+                    break;
+                case 1 :
+                    myData2Fragment = new MyData2Fragment();
+                    fragmentTransactionBack.replace(R.id.mydata_frame, myData2Fragment);
+                    fragmentTransactionBack.addToBackStack(null);
+                    fragmentTransactionBack.commitAllowingStateLoss();
+                    break;
             }
         });
+
+        next_btn.setOnClickListener(view -> {
+
+            pageNum++;
+
+            if(pageNum == 2)
+                next_btn.setText("시작");
+
+            if(pageNum > 2) {
+                Intent intent = new Intent(CollectMyDataActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            FragmentManager fragmentManagerNext = getSupportFragmentManager();
+            FragmentTransaction fragmentTransactionNext = fragmentManagerNext.beginTransaction();
+
+            System.out.println("다음 버튼 페이지 번호 : " + pageNum);
+
+            switch (pageNum) {
+                case 1 :
+                    myData2Fragment = new MyData2Fragment();
+                    fragmentTransactionNext.replace(R.id.mydata_frame, myData2Fragment);
+                    fragmentTransactionNext.addToBackStack(null);
+                    fragmentTransactionNext.commitAllowingStateLoss();
+                    break;
+                case 2 :
+                    myData3Fragment = new MyData3Fragment();
+                    fragmentTransactionNext.replace(R.id.mydata_frame, myData3Fragment);
+                    fragmentTransactionNext.addToBackStack(null);
+                    fragmentTransactionNext.commitAllowingStateLoss();
+                    break;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        FancyAlertDialog.Builder
+                .with(this)
+                .setTitle("거의 다 끝났어요!")
+                .setBackgroundColor(Color.parseColor("#303F9F"))  // for @ColorRes use setBackgroundColorRes(R.color.colorvalue)
+                .setMessage("이제 거의 다 끝났어요!")
+                .setNegativeBtnText("더 볼게요")
+                .setPositiveBtnBackground(Color.parseColor("#FF4081"))  // for @ColorRes use setPositiveBtnBackgroundRes(R.color.colorvalue)
+                .setPositiveBtnText("나갈래요")
+                .setNegativeBtnBackground(Color.parseColor("#FFA9A7A8"))  // for @ColorRes use setNegativeBtnBackgroundRes(R.color.colorvalue)
+                .setAnimation(Animation.POP)
+                .isCancellable(true)
+                .setIcon(R.drawable.ic_star_border_black_24dp, View.VISIBLE)
+                .onPositiveClicked(dialog -> finish())
+                .onNegativeClicked(dialog -> Toast.makeText(CollectMyDataActivity.this, "야호!", Toast.LENGTH_SHORT).show())
+                .build()
+                .show();
     }
 
     public void setButton() {
---
+++ app/src/main/java/com/hanium/travel/project/PreferenceManager.java
@@ -0,0 +1,77 @@
+package com.hanium.travel.project;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+class PreferenceManager {
+
+    public static final String PREFERENCES_NAME = "preference_manager";
+    private static final String DEFAULT_VALUE_STRING = "";
+    private static final boolean DEFAULT_VALUE_BOOLEAN = false;
+    private static final int DEFAULT_VALUE_INTEGER = -1;
+
+    private static SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+    }
+
+    // save String value
+    public static void setString(Context context, String key, String value) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    // save Integer value
+    public static void setInt(Context context, String key, int value) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    // save Boolean value
+    public static void setBoolean(Context context, String key, boolean value) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    // load String value
+    public static String getString(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        String value = prefs.getString(key, DEFAULT_VALUE_STRING);
+        return value;
+    }
+
+    // load Integer value
+    public static int getInt(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        int value = prefs.getInt(key, DEFAULT_VALUE_INTEGER);
+        return value;
+    }
+
+    // load Boolean value
+    public static boolean getBoolean(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        boolean value = prefs.getBoolean(key, DEFAULT_VALUE_BOOLEAN);
+        return value;
+    }
+
+    // delete key
+    public static void removeKey(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.remove(key);
+        edit.commit();
+    }
+
+    // delete data about PreperenceManager
+    public static void clear(Context context) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.clear();
+        edit.commit();
+    }
+}
